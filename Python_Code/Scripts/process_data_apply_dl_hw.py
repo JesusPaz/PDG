a3e3a07a41e15af42cb0cee194c0f98eff3c4ecd
JesusPaz <jeherpabo@gmail.com>
2019-10-01 13:34:14 -0500

    Add process script to the beats

--- Python_Code/Scripts/process_data_apply_dl_hw.py
+++ Python_Code/Scripts/process_data_apply_dl_hw.py
@@ -1,5 +1,6 @@
 import pymysql
 import operator
+import random
 
 # Constants
 sharp_dict = {}
@@ -43,6 +44,24 @@ def get_last_delay_user(user):
             query = cursor.fetchall()
 
             return query[len(query)-1]
+
+    finally:
+        connection.close()
+
+
+def get_users_from_song(song):
+    connection = pymysql.connect("127.0.0.1",
+                                 "admin",
+                                 "1539321441",
+                                 "beatsalsa")
+
+    try:
+        with connection.cursor() as cursor:
+            # Create a new record
+            sql = "SELECT `FK_CEDULA_USUARIO` FROM `databeats` WHERE `FK_ID_CANCION` = %s"
+            cursor.execute(sql, song)
+            query = cursor.fetchall()
+            return query
 
     finally:
         connection.close()
@@ -122,7 +141,6 @@ def remove_delay_hw(delay, beats):
 def delay_process_3_best():
     load_sharp_data()
     load_delay_hw()
-    get_songs_3_repetitions()
     dict_usrs = {}
     indx = 0
     num_usrs = 3
@@ -146,7 +164,12 @@ def delay_process_3_best():
         list_delay.append(dict_usrs[user])
 
     final_list = create_final_delay_list(list_delay, list_sharp)
-    print(final_list)
+    file = open("../Jupyter_Files/Data/Processed_Delay/3_Best.txt", "w")
+    msg = ""
+    for beat in final_list:
+        msg += str(beat) + "\n"
+    file.write(msg)
+    file.close()
     return final_list
 
 
@@ -174,9 +197,80 @@ def delay_process_3_worst():
         list_delay.append(dict_usrs[user])
 
     final_list = create_final_delay_list(list_delay, list_sharp)
-    print(final_list)
+    file = open("../Jupyter_Files/Data/Processed_Delay/3_Worst.txt", "w")
+    msg = ""
+    for beat in final_list:
+        msg += str(beat) + "\n"
+    file.write(msg)
+    file.close()
     return final_list
+
+
+def get_delay_hw(user_date):
+    data = user_date.split('_')
+
+    if user_date in delay_hw_ses.keys():
+        return delay_hw_ses[user_date]
+    else:
+        aux_key = ""
+        for key in delay_hw_ses.keys():
+            value = key.find(data[0])
+            if value != -1:
+                aux_key = key
+
+        return delay_hw_ses[aux_key]
+
+
+def delay_process_5_random_songs():
+
+    load_sharp_data()
+    load_delay_hw()
+    x = 0
+    number_songs = 5
+    list_songs = []
+    actual_songs = get_songs_3_repetitions()
+
+    while x < number_songs:
+        print(actual_songs[random.randint(0, len(actual_songs))])
+        song = actual_songs[random.randint(0, len(actual_songs))][0]
+        dict_usrs = {}
+        indx = 0
+        num_usrs = 3
+        aux_users = get_users_from_song(song)
+
+        print(aux_users)
+
+        x += 1
+        while indx < num_usrs:
+            print(aux_users[indx])
+            dict_usrs[aux_users[indx][0]] = 0
+            indx += 1
+
+        for usr in dict_usrs.keys():
+            query = get_song_by_user(song, usr)
+            date = query[1]
+            key = "{0}_{1}/{2}/{3}".format(str(usr), str(getattr(date, 'day')),
+                                           str(getattr(date, 'month')), str(getattr(date, 'year')))
+
+            delay = get_delay_hw(key)
+            dict_usrs[usr] = remove_delay_hw(delay, query[0])
+
+        list_sharp = []
+        list_delay = []
+        for user in dict_usrs.keys():
+            list_sharp.append(sharp_dict[str(user)])
+            list_delay.append(dict_usrs[user])
+
+        final_list = create_final_delay_list(list_delay, list_sharp)
+        file = open("../Jupyter_Files/Data/Processed_Songs/"+str(song)+".txt", "w")
+        msg = ""
+        for beat in final_list:
+            msg += str(beat)+"\n"
+        file.write(msg)
+        file.close()
+        print(final_list)
 
 
 delay_process_3_best()
 delay_process_3_worst()
+delay_process_5_random_songs()
